e256972944fbe7d09a54604d943f7c1a03cc9388
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-03-13 06:13:20 +0000

    added some error handeling

--- app.js
+++ app.js
@@ -7,4 +7,22 @@ const albumRoutes = require('./api/routes/albums');
 app.use('/songs', songRoutes);
 app.use('/albums', albumRoutes);
 
+// error handeling
+app.use((req, res, next) => {
+    const error = new Error('Error Occured');  
+    // 404 code is used if didn't find a route  
+    error.status = 404;
+    next(error);
+});
+
+// error catching for problems in database 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message : error.message 
+        }
+    })
+});
+
 module.exports = app;
