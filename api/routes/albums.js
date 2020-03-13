1a0990ec7a2d81e8cc419a0caad48797d350a4f2
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-03-13 20:27:56 +0000

    commented out post patch and delete

--- api/routes/albums.js
+++ api/routes/albums.js
@@ -7,22 +7,22 @@ router.get('/', (req, res, next) => {
     });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        message : 'Handle POST requests to /albums'
-    });
-});
+// router.post('/', (req, res, next) => {
+//     res.status(200).json({
+//         message : 'Handle POST requests to /albums'
+//     });
+// });
 
-router.patch('/', (req, res, next) => {
-    res.status(200).json({
-        message : 'Handle PATCH requests to /albums'
-    });
-});
+// router.patch('/', (req, res, next) => {
+//     res.status(200).json({
+//         message : 'Handle PATCH requests to /albums'
+//     });
+// });
 
-router.delete('/', (req, res, next) => {
-    res.status(200).json({
-        message : 'Handle DELETE requests to /albums'
-    });
-});
+// router.delete('/', (req, res, next) => {
+//     res.status(200).json({
+//         message : 'Handle DELETE requests to /albums'
+//     });
+// });
 
 module.exports = router;
