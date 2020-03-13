086cc0f7efe9ce5d855588a22b4f89b7223255bb
OrionMat <36226295+OrionMat@users.noreply.github.com>
2020-03-13 05:36:33 +0000

    added routes for post. patch and delete

--- api/routes/songs.js
+++ api/routes/songs.js
@@ -7,4 +7,22 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.post('/', (req, res, next) => {
+    res.status(200).json({
+        message : 'Handle POST requests to /songs'
+    });
+});
+
+router.patch('/', (req, res, next) => {
+    res.status(200).json({
+        message : 'Handle PATCH requests to /songs'
+    });
+});
+
+router.delete('/', (req, res, next) => {
+    res.status(200).json({
+        message : 'Handle DELETE requests to /songs'
+    });
+});
+
 module.exports = router;
